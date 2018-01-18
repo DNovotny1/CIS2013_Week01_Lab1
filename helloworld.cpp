d514e83f893d0da8316ebe087fd697c4cd65667e
Daniel Novotny <svc_okc_bt204@ad.osuokc.edu>
2018-01-17 20:38:56 -0600

    hello world
    c++ hello world and compiled
    week one in CIS2013

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char something;
+	cout << "type something";
+	cin >> something;
+	cout << "you typed something " << something;
+	return 0;
+}
